0f55e70b0015a692f23c86c6bb3fa98b5b6f3e5b
agnno <agnnow03@gmail.com>
2026-06-17 20:43:29 +0200

    added stuff for segmenter with rules for languages

---
+++ segmentation/engine/segmenter.py
@@ -0,0 +1,8 @@
+import re
+
+class Segmenter:
+    def __init__(self):
+        pass
+
+
+    def segment(text):
---
+++ segmentation/infrastructure/loaders/yaml_rule_loader.py
@@ -0,0 +1,5 @@
+class YamlRuleLoader:
+    def __init__(self):
+        pass
+
+    
